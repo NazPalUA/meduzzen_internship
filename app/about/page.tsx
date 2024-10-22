13afa412d8d35295dfad109fe15ba99c09e9135b
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-10-22 11:01:14 +0300

    refactor(constants): extract static texts and routes into constants
    Moved static texts to 'texts.ts' and routes to 'routes.ts'. Updated 'app/page.tsx' and 'app/about/page.tsx' accordingly.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,13 @@
+import { ABOUT_PAGE_TEXT } from "@/src/shared/constants/texts"
 import { Container, Typography } from "@mui/material"
 
 export default function About() {
 	return (
 		<Container maxWidth="sm" style={{ textAlign: "center", marginTop: "50px" }}>
 			<Typography variant="h4" component="h1" gutterBottom>
-				About This Project
+				{ABOUT_PAGE_TEXT.TITLE}
 			</Typography>
-			<Typography variant="body1">
-				This project is part of the Meduzzen internship program.
-			</Typography>
+			<Typography variant="body1">{ABOUT_PAGE_TEXT.DESCRIPTION}</Typography>
 		</Container>
 	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
+import { Routes } from "@/src/shared/constants/routes"
+import { HOME_PAGE_TEXT } from "@/src/shared/constants/texts"
 import { Button, Container, Typography } from "@mui/material"
 
 export default function Home() {
 	return (
 		<Container maxWidth="sm" style={{ textAlign: "center", marginTop: "50px" }}>
 			<Typography variant="h2" component="h1" gutterBottom>
-				Meduzzen Intership
+				{HOME_PAGE_TEXT.TITLE}
 			</Typography>
 			<Typography variant="h5" component="p" gutterBottom>
-				Welcome to our project!
+				{HOME_PAGE_TEXT.WELCOME_MESSAGE}
 			</Typography>
-			<Button variant="contained" color="primary" href="/about">
-				Learn More
+			<Button variant="contained" color="primary" href={Routes.ABOUT}>
+				{HOME_PAGE_TEXT.LEARN_MORE_BUTTON}
 			</Button>
 		</Container>
 	)
