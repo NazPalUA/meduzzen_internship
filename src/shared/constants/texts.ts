572f52fe7276b5b109ce5e3681af011822233524
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-10-23 23:28:57 +0300

    refactor(constants): Update text constants to use enums

--- src/shared/constants/texts.ts
+++ src/shared/constants/texts.ts
@@ -1,45 +1,45 @@
-export const HEADER_TEXT = {
-	TITLE: "Meduzzen Internship",
-	HOME_LINK_LABEL: "Home",
-	ABOUT_LINK_LABEL: "About",
-	USERS_LINK_LABEL: "Users",
-	COMPANIES_LINK_LABEL: "Companies",
+export enum HEADER_TEXT {
+	TITLE = "Meduzzen Internship",
+	HOME_LINK_LABEL = "Home",
+	ABOUT_LINK_LABEL = "About",
+	USERS_LINK_LABEL = "Users",
+	COMPANIES_LINK_LABEL = "Companies",
 }
 
-export const FOOTER_TEXT = {
-	COPYRIGHT: "Meduzzen Internship Project",
-	PRIVACY_POLICY_LINK_LABEL: "Privacy Policy",
-	TERMS_OF_SERVICE_LINK_LABEL: "Terms of Service",
+export enum FOOTER_TEXT {
+	COPYRIGHT = "Meduzzen Internship Project",
+	PRIVACY_POLICY_LINK_LABEL = "Privacy Policy",
+	TERMS_OF_SERVICE_LINK_LABEL = "Terms of Service",
 }
 
-export const HOME_PAGE_TEXT = {
-	TITLE: "Meduzzen Intership",
-	WELCOME_MESSAGE: "Welcome to our project!",
-	LEARN_MORE_BUTTON: "Learn More",
+export enum HOME_PAGE_TEXT {
+	TITLE = "Meduzzen Intership",
+	WELCOME_MESSAGE = "Welcome to our project!",
+	LEARN_MORE_BUTTON = "Learn More",
 }
 
-export const ABOUT_PAGE_TEXT = {
-	TITLE: "About This Project",
-	DESCRIPTION: "This project is part of the Meduzzen internship program.",
+export enum ABOUT_PAGE_TEXT {
+	TITLE = "About This Project",
+	DESCRIPTION = "This project is part of the Meduzzen internship program.",
 }
 
-export const USERS_PAGE_TEXT = {
-	TITLE: "Our Community",
-	DESCRIPTION: "Meet the talented individuals in our network.",
+export enum USERS_PAGE_TEXT {
+	TITLE = "Our Community",
+	DESCRIPTION = "Meet the talented individuals in our network.",
 }
 
-export const COMPANIES_PAGE_TEXT = {
-	TITLE: "Partner Companies",
-	DESCRIPTION: "Discover the innovative companies we work with.",
+export enum COMPANIES_PAGE_TEXT {
+	TITLE = "Partner Companies",
+	DESCRIPTION = "Discover the innovative companies we work with.",
 }
 
-export const PROFILE_PAGE_TEXT = {
-	TITLE: "Profile",
-	EMAIL: "Email",
-	ABOUT: "About",
+export enum PROFILE_PAGE_TEXT {
+	TITLE = "Profile",
+	EMAIL = "Email",
+	ABOUT = "About",
 }
 
-export const COMPANY_PROFILE_PAGE_TEXT = {
-	TITLE: "Company Profile",
-	DESCRIPTION: "Description",
+export enum COMPANY_PROFILE_PAGE_TEXT {
+	TITLE = "Company Profile",
+	DESCRIPTION = "Description",
 }
