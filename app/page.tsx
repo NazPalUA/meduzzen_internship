744d2ede7201595a11b9fa6f25111f813bbd6ed3
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-10-23 20:43:32 +0300

    feat(modal): Add Modal component
    - Add reusable Modal component
    - Add temporary LearnMore component to show how to use Modal component
    - Added LearnMore to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Routes } from "@/src/shared/constants/routes"
 import { HOME_PAGE_TEXT } from "@/src/shared/constants/texts"
 import { PageContainer } from "@/src/shared/ui/PageContainer"
-import { Button, Typography } from "@mui/material"
+import { Typography } from "@mui/material"
+import LearnMore from "./_lib/LearnMore"
 
 export default function Home() {
 	return (
@@ -12,9 +12,7 @@ export default function Home() {
 			<Typography variant="h5" component="p" gutterBottom>
 				{HOME_PAGE_TEXT.WELCOME_MESSAGE}
 			</Typography>
-			<Button variant="contained" color="primary" href={Routes.ABOUT}>
-				{HOME_PAGE_TEXT.LEARN_MORE_BUTTON}
-			</Button>
+			<LearnMore />
 		</PageContainer>
 	)
 }
---
+++ src/shared/ui/Modal.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import {
+	Box,
+	Button,
+	Modal as MuiModal,
+	Typography,
+	useTheme,
+} from "@mui/material"
+
+const style = {
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: "90%",
+	maxWidth: 500,
+	bgcolor: "background.paper",
+	borderRadius: 8,
+	boxShadow: 24,
+	p: 4,
+	transition: "opacity 0.3s ease-in-out",
+}
+
+type Props = {
+	open: boolean
+	onClose: () => void
+	title: string
+	content: React.ReactNode
+}
+
+export function Modal({ open, onClose, title, content }: Props) {
+	const theme = useTheme()
+
+	return (
+		<MuiModal
+			open={open}
+			onClose={onClose}
+			aria-labelledby="modal-title"
+			aria-describedby="modal-description"
+		>
+			<Box sx={style} role="dialog" aria-modal="true">
+				<Typography
+					id="modal-title"
+					variant="h6"
+					component="h2"
+					tabIndex={-1}
+					sx={{ color: theme.palette.text.primary, fontWeight: "bold" }}
+				>
+					{title}
+				</Typography>
+				<Typography
+					id="modal-description"
+					sx={{ mt: 2, color: theme.palette.text.secondary }}
+				>
+					{content}
+				</Typography>
+				<Button
+					onClick={onClose}
+					variant="contained"
+					color="primary"
+					sx={{ mt: 2 }}
+				>
+					Close
+				</Button>
+			</Box>
+		</MuiModal>
+	)
+}
