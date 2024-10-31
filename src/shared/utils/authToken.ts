a25ddf0a53338ebf443175170fcf223e83821a29
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-10-31 17:58:00 +0200

    feat(auth): add token expiration check
    - Add "jwt-decode" dependency
    - Implemented a function to verify token expiration
    - Ensures users are redirected to the login page if the token is expired

--- src/shared/utils/authToken.ts
+++ src/shared/utils/authToken.ts
@@ -1,6 +1,17 @@
+import { jwtDecode } from "jwt-decode"
+import { Routes } from "../constants/routes"
+
 export const getToken = (): string | null => {
   if (typeof window !== "undefined") {
-    return localStorage.getItem("token")
+    const token = localStorage.getItem("token")
+    if (token) {
+      if (isTokenExpired(token)) {
+        removeToken()
+        window.location.href = Routes.LOGIN
+        return null
+      }
+      return token
+    }
   }
   return null
 }
@@ -14,5 +25,14 @@ export const setToken = (token: string): void => {
 export const removeToken = (): void => {
   if (typeof window !== "undefined") {
     localStorage.removeItem("token")
+  }
+}
+
+function isTokenExpired(token: string): boolean {
+  try {
+    const decodedToken = jwtDecode<{ exp: number }>(token)
+    return decodedToken.exp < Date.now() / 1000
+  } catch {
+    return true
   }
 }
