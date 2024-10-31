747b7fa1b93df03e710cacbdf46d1aa4cd6d0c84
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-10-31 14:23:46 +0200

    refactor: update redux store config to follow approach from docs

--- src/shared/store/store.ts
+++ src/shared/store/store.ts
@@ -1,4 +1,5 @@
 import { testSliceReducer } from "@features/test-store"
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 
 const rootReducer = combineSlices({
@@ -8,9 +9,18 @@ const rootReducer = combineSlices({
 export const makeStore = () => {
   return configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => {
+      return getDefaultMiddleware()
+    },
   })
 }
 
 export type RootState = ReturnType<typeof rootReducer>
 export type AppStore = ReturnType<typeof makeStore>
 export type AppDispatch = AppStore["dispatch"]
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>
