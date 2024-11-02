86d6f0aecb6095402aa2439c7693280b348c12fd
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-02 03:28:22 +0200

    fix(auth): remove redirect from sessionMiddleware.
    Redirect is already handled by useSession hook

--- src/entities/session/store/sessionMiddleware.ts
+++ src/entities/session/store/sessionMiddleware.ts
@@ -1,13 +1,9 @@
 import { Middleware } from "@reduxjs/toolkit"
-import { Routes } from "@shared/constants/routes"
 import { removeToken } from "@shared/utils/authToken"
 
 export const sessionMiddleware: Middleware = () => (next) => (action: any) => {
   if (action.type.endsWith("/rejected") && action.payload?.status === 401) {
     removeToken()
-    if (typeof window !== "undefined") {
-      window.location.href = Routes.LOGIN
-    }
   }
   return next(action)
 }
