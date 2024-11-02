1174d01fd54158c81f0d12aa6726f81d87241788
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-02 14:24:46 +0200

    feat(i18n): add translation to api-health-check

--- src/features/api-health-check/ui/ApiHealthCheck.tsx
+++ src/features/api-health-check/ui/ApiHealthCheck.tsx
@@ -1,26 +1,29 @@
 "use client"
 
 import { Alert, Button, CircularProgress } from "@mui/material"
+import { useTranslations } from "next-intl"
 import { useCheckHealthQuery } from "../store/healthApiSlice"
 import styles from "./ApiHealthCheck.module.scss"
 
 export const ApiHealthCheck = () => {
   const { isLoading, error, refetch, isFetching, isSuccess } = useCheckHealthQuery()
-
+  const t = useTranslations("ApiHealthCheck")
   return (
     <div className={styles.container}>
       <Button variant="contained" onClick={() => refetch()} disabled={isLoading}>
-        Check API Health
+        {t("checkButton")}
       </Button>
 
       <div className={styles.status}>
         {isFetching && <CircularProgress />}
 
         {error && !isFetching && (
-          <Alert severity="error">{error instanceof Error ? error.message : "Unknown error"}</Alert>
+          <Alert severity="error">
+            {error instanceof Error ? error.message : t("errorMessage")}
+          </Alert>
         )}
 
-        {isSuccess && !isFetching && <Alert severity="success">API is healthy!</Alert>}
+        {isSuccess && !isFetching && <Alert severity="success">{t("successMessage")}</Alert>}
       </div>
     </div>
   )
