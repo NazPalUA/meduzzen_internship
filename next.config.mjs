d36ec6c2e67f64873b6639e5cb1805796b0e3e20
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-04 03:07:45 +0200

    feat: update next config to allow images from "51.20.210.187" host

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const withNextIntl = createNextIntlPlugin("./src/shared/i18n/request.ts")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "51.20.210.187",
+      },
+    ],
+  },
 }
 
 export default withNextIntl(nextConfig)
