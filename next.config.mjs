f442178935a6abd4ff341c02c0d0fac9b48fcfc8
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-05 21:08:34 +0200

    fix: eslint error

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,20 @@ import createNextIntlPlugin from "next-intl/plugin"
 
 const withNextIntl = createNextIntlPlugin("./src/shared/i18n/request.ts")
 
+if (!process.env.NEXT_PUBLIC_API_URL) {
+  throw new Error("NEXT_PUBLIC_API_URL is not defined in the environment variables")
+}
+
+const url = new URL(process.env.NEXT_PUBLIC_API_URL)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: process.env.NEXT_PUBLIC_API_URL?.replace("http://", ""),
+        protocol: url.protocol.replace(":", ""),
+        hostname: url.hostname,
       },
     ],
   },
