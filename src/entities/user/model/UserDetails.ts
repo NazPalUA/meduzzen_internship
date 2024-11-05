09e2c64d1816779aab7aa4ffe43a8cd84f3ec5f1
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-05 20:30:31 +0200

    refactor: remove duplication of UserDetails Schema definition

--- src/entities/user/model/UserDetails.ts
+++ src/entities/user/model/UserDetails.ts
@@ -1,20 +1,9 @@
 import { ServerResponseSchema } from "@shared/models/ServerResponseSchema"
+import { UserDetailsSchema } from "@shared/models/UserDetails"
 import { z } from "zod"
 
-export const UserDetailsSchema = z.object({
-  user_id: z.number(),
-  user_email: z.string().email(),
-  user_firstname: z.string(),
-  user_lastname: z.string(),
-  user_avatar: z.string().nullable(),
-  user_status: z.string().nullable(),
-  user_city: z.string().nullable(),
-  user_phone: z.string().nullable(),
-  user_links: z.array(z.string()).nullable(),
-  is_superuser: z.boolean(),
-})
+export { UserDetailsSchema, type UserDetails } from "@shared/models/UserDetails"
 
 export const UserDetailsResponseSchema = ServerResponseSchema(UserDetailsSchema)
 
-export type UserDetails = z.infer<typeof UserDetailsSchema>
 export type UserDetailsResponse = z.infer<typeof UserDetailsResponseSchema>
