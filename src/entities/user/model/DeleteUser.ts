3827fbf3438ae0a35e4791e7355a32c1087f2432
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-05 13:51:57 +0200

    fix: update DeleteUserResponseSchema

--- src/entities/user/model/DeleteUser.ts
+++ src/entities/user/model/DeleteUser.ts
@@ -3,7 +3,7 @@ import { z } from "zod"
 export const DeleteUserResponseSchema = z.object({
   status_code: z.number(),
   detail: z.string(),
-  result: z.string(),
+  result: z.string().or(z.null()),
 })
 
 export type DeleteUserResponse = z.infer<typeof DeleteUserResponseSchema>
