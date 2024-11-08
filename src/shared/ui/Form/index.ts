c2e093de9b85cbb289bbb7387e0032e08f1a4b01
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-08 02:59:18 +0200

    feat: add switch to the form component

--- src/shared/ui/Form/index.ts
+++ src/shared/ui/Form/index.ts
@@ -3,11 +3,13 @@ import { ArrayField, ArrayFieldProps } from "./ArrayField"
 import { ErrorMessage, type ErrorMessageProps } from "./ErrorMessage"
 import { Form, type FormProps } from "./Form"
 import { SubmitButton, type SubmitButtonProps } from "./SubmitButton"
+import { Switch, SwitchProps } from "./Switch"
 import { TextField, type TextFieldProps } from "./TextField"
 
 type CompoundForm<T extends FieldValues> = React.FC<FormProps<T>> & {
   TextField: React.FC<TextFieldProps<T>>
   ArrayField: React.FC<ArrayFieldProps<T>>
+  Switch: React.FC<SwitchProps<T>>
   SubmitButton: React.FC<SubmitButtonProps>
   ErrorMessage: React.FC<ErrorMessageProps>
 }
@@ -17,6 +19,7 @@ export const getForm = <T extends FieldValues>() => {
 
   FormComponent.TextField = TextField<T>
   FormComponent.ArrayField = ArrayField<T>
+  FormComponent.Switch = Switch<T>
   FormComponent.SubmitButton = SubmitButton
   FormComponent.ErrorMessage = ErrorMessage
 
