03a5540136bb46f016bcde1b01b62307143bc1db
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-12 15:13:29 +0200

    feat: add UserRequests and UserInvites to Dashboard with fake data

--- src/widgets/Dashboard/ui/Dashboard.tsx
+++ src/widgets/Dashboard/ui/Dashboard.tsx
@@ -6,6 +6,8 @@ import { ContentDialog } from "@shared/components/ui"
 import { useDialog } from "@shared/hooks"
 import { useTranslations } from "next-intl"
 import { CreateCompanyForm } from "./CreateCompanyForm"
+import { UserInvites } from "./UserInvites"
+import { UserRequests } from "./UserRequests"
 
 export function Dashboard() {
   const t = useTranslations("CreateCompany")
@@ -17,18 +19,24 @@ export function Dashboard() {
   }
 
   return (
-    <Button
-      variant="contained"
-      color="primary"
-      onClick={() =>
-        openDialog(
-          <ContentDialog title={t("modal.title")}>
-            <CreateCompanyForm />
-          </ContentDialog>,
-        )
-      }
-    >
-      {t("modal.openButton")}
-    </Button>
+    <div>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() =>
+          openDialog(
+            <ContentDialog title={t("modal.title")}>
+              <CreateCompanyForm />
+            </ContentDialog>,
+          )
+        }
+      >
+        {t("modal.openButton")}
+      </Button>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <UserInvites user={currentUser} />
+        <UserRequests user={currentUser} />
+      </div>
+    </div>
   )
 }
