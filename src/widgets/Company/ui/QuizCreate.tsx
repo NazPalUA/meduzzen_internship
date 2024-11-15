f9f6cf6c90fe6033739b6029638bc6f55b1e3d29
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-15 09:12:20 +0200

    refactor: QuizCreate

--- src/widgets/Company/ui/QuizCreate.tsx
+++ src/widgets/Company/ui/QuizCreate.tsx
@@ -1,35 +1,21 @@
 "use client "
 
+import { ContentDialog } from "@/src/shared/components/ui"
 import { CreateQuizForm } from "@entities/quiz"
 import { Quiz as QuizIcon } from "@mui/icons-material"
-import CloseIcon from "@mui/icons-material/Close"
-import { Button, DialogContent, IconButton } from "@mui/material"
+import { Button } from "@mui/material"
 import { useDialog } from "@shared/hooks"
 import { useTranslations } from "next-intl"
 
 export function QuizCreate({ companyId }: { companyId: number }) {
   const t = useTranslations("CompanyPage.quizzes")
 
-  const { openDialog, closeDialog } = useDialog()
+  const { openDialog } = useDialog()
 
   const createQuizDialog = (
-    <>
-      <IconButton
-        aria-label="close"
-        onClick={closeDialog}
-        sx={(theme) => ({
-          position: "absolute",
-          right: 8,
-          top: 8,
-          color: theme.palette.grey[500],
-        })}
-      >
-        <CloseIcon />
-      </IconButton>
-      <DialogContent>
-        <CreateQuizForm companyId={companyId} />
-      </DialogContent>
-    </>
+    <ContentDialog>
+      <CreateQuizForm companyId={companyId} />
+    </ContentDialog>
   )
 
   return (
