424648e2b899be22836006f506ee21c7fbec22d5
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-16 19:25:43 +0200

    feat: add link to my companies and refactor user navbar

--- src/widgets/Header/ui/Auth.tsx
+++ src/widgets/Header/ui/Auth.tsx
@@ -3,57 +3,86 @@
 import { useSession } from "@entities/session"
 import LoginIcon from "@mui/icons-material/Login"
 import PersonIcon from "@mui/icons-material/Person"
+import { IconButton, Menu, MenuItem } from "@mui/material"
 import { Link } from "@navigation"
+import { Avatar } from "@shared/components/ui"
 import { Routes } from "@shared/constants"
 import { useTranslations } from "next-intl"
 import { useState } from "react"
-import styles from "./Auth.module.scss"
+
+const AuthIcon = ({ user, isLoggedIn }: { user: any; isLoggedIn: boolean }) => {
+  if (!isLoggedIn) return <LoginIcon />
+
+  if (user?.user_avatar) {
+    return <Avatar size="xs" alt={user?.user_firstname} src={user?.user_avatar} />
+  }
+
+  return <PersonIcon />
+}
 
 export function Auth() {
   const t = useTranslations("Navigation")
   const { isLoggedIn, user } = useSession()
-  const [isOpen, setIsOpen] = useState(false)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
 
-  const toggleDropdown = () => {
-    setIsOpen((prevState) => !prevState)
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
   }
 
-  const closeDropdown = () => {
-    setIsOpen(false)
+  const handleMenuClose = () => {
+    setAnchorEl(null)
   }
 
   return (
-    <div className={styles.authContainer}>
-      <button className={styles.iconButton} onClick={toggleDropdown}>
-        {isLoggedIn ? <PersonIcon /> : <LoginIcon />}
-      </button>
-      {isOpen && (
-        <div className={styles.dropdownMenu}>
-          {isLoggedIn && user ? (
-            <>
-              <Link href={Routes.LOGOUT} className={styles.menuItem} onClick={closeDropdown}>
-                {t("logout")}
-              </Link>
-              <Link
-                href={`${Routes.USERS}/${user.user_id}`}
-                className={styles.menuItem}
-                onClick={closeDropdown}
-              >
-                {t("myProfile")}
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link href={Routes.LOGIN} className={styles.menuItem} onClick={closeDropdown}>
-                {t("login")}
-              </Link>
-              <Link href={Routes.SIGN_UP} className={styles.menuItem} onClick={closeDropdown}>
-                {t("signUp")}
-              </Link>
-            </>
-          )}
-        </div>
-      )}
+    <div>
+      <IconButton onClick={handleMenuOpen} color="primary">
+        <AuthIcon user={user} isLoggedIn={isLoggedIn} />
+      </IconButton>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        {isLoggedIn && user ? (
+          <>
+            <MenuItem component={Link} href={Routes.LOGOUT} onClick={handleMenuClose}>
+              {t("logout")}
+            </MenuItem>
+            <MenuItem
+              component={Link}
+              href={`${Routes.USERS}/${user.user_id}`}
+              onClick={handleMenuClose}
+            >
+              {t("myProfile")}
+            </MenuItem>
+            <MenuItem
+              component={Link}
+              href={`${Routes.USERS}/${user.user_id}?tab=companies`}
+              onClick={handleMenuClose}
+            >
+              {t("myCompanies")}
+            </MenuItem>
+          </>
+        ) : (
+          <>
+            <MenuItem component={Link} href={Routes.LOGIN} onClick={handleMenuClose}>
+              {t("login")}
+            </MenuItem>
+            <MenuItem component={Link} href={Routes.SIGN_UP} onClick={handleMenuClose}>
+              {t("signUp")}
+            </MenuItem>
+          </>
+        )}
+      </Menu>
     </div>
   )
 }
