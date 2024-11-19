9cc27227626d98d5cda8c39ef6374d9ea27c8a68
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-11-20 00:21:44 +0200

    refactor: CompanyDataSummaryRatingAnalyticForUsersResponseSchema

--- src/features/company-data/model/getSummaryRatingAnalyticForUsers.ts
+++ src/features/company-data/model/getSummaryRatingAnalyticForUsers.ts
@@ -1,16 +1,18 @@
 import { RatingSchema, ServerResponseSchema } from "@shared/api"
 import { z } from "zod"
 
+export const AllUsersAnalyticsSchema = z.object({
+  user_id: z.number(),
+  rating: z.array(RatingSchema),
+})
+
 export const CompanyDataSummaryRatingAnalyticForUsersResponseSchema = ServerResponseSchema(
   z.object({
-    rating: z.array(
-      z.object({
-        rating: z.array(RatingSchema),
-        user_id: z.number(),
-      }),
-    ),
+    rating: z.array(AllUsersAnalyticsSchema),
   }),
 )
+
+export type AllUsersAnalytics = z.infer<typeof AllUsersAnalyticsSchema>
 
 export type CompanyDataSummaryRatingAnalyticForUsersResponse = z.infer<
   typeof CompanyDataSummaryRatingAnalyticForUsersResponseSchema
